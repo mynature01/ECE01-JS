d8ec931db5af4df60a2bdb8e789c5085186de5f9
sanskargaur318-crypto <sanskargaur318@gmail.com>
2026-02-25 10:14:36 +0530

    adding new code

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-console.log("Hello World");
-        let a = 10;
-        console.log(a);
-        console.log(typeof(a));
-        a=false;
-        console.log(a);
-        console.log(typeof(a));
-        a="Hello World";
-        console.log(a);
-        console.log(typeof(a));
-        a=null;
-        console.log(a);
-        console.log(typeof(a));
+// console.log("Hello World");
+//         let a = 10;
+//         console.log(a);
+//         console.log(typeof(a));
+//         a=false;
+//         console.log(a);
+//         console.log(typeof(a));
+//         a="Hello World";
+//         console.log(a);
+//         console.log(typeof(a));
+//         a=null;
+//         console.log(a);
+//         console.log(typeof(a));
+
+        const sym1 = Symbol(4);
+        console.log(sym1);
+        const sym2 = Symbol(4);
+        console.log(sym2);
+        if(sym1==sym2){
+            console.log("true");
+        }
+        else{
+            console.log("false")
+        }
+        
