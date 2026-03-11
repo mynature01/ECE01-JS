415059296187fa3bf8e4b019455fbc6f5d29175b
sanskargaur318-crypto <sanskargaur318@gmail.com>
2026-03-11 10:30:31 +0530

    study about functions

--- doc.js
+++ doc.js
@@ -27,16 +27,51 @@
 //     document.write("You are not eligible to vote");
 // }
 
-let grade = 'A';
-document.write("Entering switch statement"+"<br/>");
-switch (grade) {
-    case 'A':document.write("Excellent");
-    break;
-    case 'B':document.write("Good");
-    break;
-    case 'C':document.write("Fair");
-    break;
-    case 'D':document.write("Poor");
-    break;
-    default:document.write("Invalid grade");
-}
+// let grade = 'A';
+// document.write("Entering switch statement"+"<br/>");
+// switch (grade) {
+//     case 'A':document.write("Excellent");
+//     break;
+//     case 'B':document.write("Good");
+//     break;
+//     case 'C':document.write("Fair");
+//     break;
+//     case 'D':document.write("Poor");
+//     break;
+//     default:document.write("Invalid grade");
+// }
+
+// function myFunction() {
+//     alert("Button clicked");
+// }
+
+// function sayHello(name,age){
+//     document.write("Hello "+name+"! You are "+age+" years old.");
+// }
+
+// var add2 = (a, b) =>
+//     console.log(a + b);
+// add2(5, 10);
+
+// var person = {
+//     name: "Sanskar",
+//     age: 19,
+//     isMarried: false,
+//     address: {
+//         street: "teacher colony",
+//         city: "REWA",
+//     }
+// };
+// console.log(person);
+
+var person2 = new Object();
+person2.name = "Sanskar";
+person2.age = 19;
+person2.isMarried = false;
+person2.address = {};
+person2.address.street = "teacher colony";
+person2['address']['city'] = "REWA";
+console.log(person2);
+
+
+
