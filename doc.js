18c898b7a5cef303819740e3994507fa0950bd05
sanskargaur318-crypto <sanskargaur318@gmail.com>
2026-03-11 09:51:40 +0530

    learning the loops and conditional statement

---
+++ doc.js
@@ -0,0 +1,42 @@
+// let count;
+// document.write("Starting loop"+"<br/>");
+// for(count=0;count<10;count++){
+//     document.write("Current count:"+count + "<br/>");
+
+// }
+// document.write("Loop stopped")
+
+// for(key in object){
+//code block to be executed
+// }
+// const person={
+//     fname:"Sanskar",
+//     lname:"Gaur",
+//     age:19
+
+// };
+// for (let x in person){
+//     document.write("person details:" + x + ":" + person[x] + "<br/>");
+// }
+
+// var age = 19;
+// if (age >= 18) {
+//     document.write("You are eligible to vote");
+// }
+// else {
+//     document.write("You are not eligible to vote");
+// }
+
+let grade = 'A';
+document.write("Entering switch statement"+"<br/>");
+switch (grade) {
+    case 'A':document.write("Excellent");
+    break;
+    case 'B':document.write("Good");
+    break;
+    case 'C':document.write("Fair");
+    break;
+    case 'D':document.write("Poor");
+    break;
+    default:document.write("Invalid grade");
+}
