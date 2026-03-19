cae24957fd424cc00da860e3c9e9b383b23af8a3
sanskargaur318-crypto <sanskargaur318@gmail.com>
2026-03-19 11:47:26 +0530

    studying java of events

---
+++ events.js
@@ -0,0 +1,42 @@
+// const result = document.getElementById("sk");
+// // console.log(result);
+// result.innerHTML = "<b>Hello World</b>";
+// const result1 = document.getElementsByClassName("sk1");
+// // console.log(result1);
+// result1[1].innerHTML="Gaur"
+// result1[0].innerHTML="Sanskar"
+
+// const result2 = document.getElementsByTagName("p");
+// // console.log(result2);
+// result2[0].innerHTML="This is a paragraph"
+// result2[1].innerHTML="This is another paragraph"
+// result2[1].style.color="red"
+// result2[1].style.fontSize="20px"
+
+// const result = document.getElementById("sk").getAttribute("class");
+// document.getElementById("p1").innerHTML=result;
+// document.getElementById("p2").innerHTML=result;
+
+function onClick(){
+    console.log("Button clicked");
+}
+
+function onMouseOver(){
+    console.log("Mouse over");
+}
+
+function onMouseOut(){
+    console.log("Mouse out");
+}  
+function onMouseDown() {
+    console.log("Mouse down");
+}
+function onMouseUp() {
+    console.log("Mouse up");
+}
+function onMouseMove() {
+    console.log("Mouse move");
+}
+function onDblClick() {
+    console.log("Double click");
+}
