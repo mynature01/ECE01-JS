e8cfd6590a3e931afab6f1fea57d498da8dcfd13
Sanskar-56 <sanskargaur318@gmail.com>
2026-06-19 10:33:28 +0530

    external practical

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// console.log("Hello World");
-//         let a = 10;
-//         console.log(a);
-//         console.log(typeof(a));
-//         a=false;
-//         console.log(a);
-//         console.log(typeof(a));
-//         a="Hello World";
-//         console.log(a);
-//         console.log(typeof(a));
-//         a=null;
-//         console.log(a);
-//         console.log(typeof(a));
+function findLargest() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    let n3 = Number(document.getElementById("num3").value);
+    let n4 = Number(document.getElementById("num4").value);
+    let n5 = Number(document.getElementById("num5").value);
 
-        // const sym1 = Symbol(4);
-        // console.log(sym1);
-        // const sym2 = Symbol(4);
-        // console.log(sym2);
-        // if(sym1==sym2){
-        //     console.log("true");
-        // }
-        // else{
-        //     console.log("false")
-        // }
+    let largest;
 
-        var myInfo = new Array();
-        var myInfo1 = Array();
-        console.log(myInfo,myInfo1);
-        
+    if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5) {
+        largest = n1;
+    }
+    else if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5) {
+        largest = n2;
+    }
+    else if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5) {
+        largest = n3;
+    }
+    else if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5) {
+        largest = n4;
+    }
+    else {
+        largest = n5;
+    }
+
+    document.getElementById("result").innerHTML =
+        "Largest Number is: " + largest;
+}
